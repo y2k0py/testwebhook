c03908793a05d909486be74268130408ac7fc2a2
Ivan Petrovsky <47870638+y2k0py@users.noreply.github.com>
2023-11-21 22:17:35 +0200

    Update get_hooks.py

--- get_hooks.py
+++ get_hooks.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import ssl
 
 app = Flask(__name__)
 # Обработчик входящих вебхуков
@@ -14,6 +13,4 @@ def webhook():
     return "Invalid request method"
 
 if __name__ == '__main__':
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    ssl_context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
     app.run(host='0.0.0.0', port=8443, ssl_context=ssl_context)
