b0e016e5f5cc52b965b04ed86c4c1b7696c3b1d8
Ivan Petrovsky <47870638+y2k0py@users.noreply.github.com>
2023-11-21 22:23:43 +0200

    Update get_hooks.py

--- get_hooks.py
+++ get_hooks.py
@@ -13,4 +13,4 @@ def webhook():
     return "Invalid request method"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8443, ssl_context=ssl_context)
+    app.run(host='0.0.0.0', port=8443)
